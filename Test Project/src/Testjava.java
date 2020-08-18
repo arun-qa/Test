67fc00e344f20b12b2007dd5e4ec63a15ed020f1
arun-qa <mailtoarun1911@gmail.com>
2020-08-18 17:23:47 +0530

    commit 3

--- Test Project/src/Testjava.java
+++ Test Project/src/Testjava.java
@@ -1,8 +1,14 @@
 
 public class Testjava {
+	
+	static int a=5;
+	static int b=6;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int c=a+b;
+		
+		System.out.println(c);
 
 	}
 
